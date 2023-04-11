a9a59327c52759abc022751d9b7cb9a2f4820f6c
Roberto Valdez <roberto.valdezisc@outlook.com>
2023-04-11 15:21:50 -0600

    add filters for authenticate users

---
+++ src/main/java/com/scurity/jwt/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.scurity.jwt.auth;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+
+
+}
