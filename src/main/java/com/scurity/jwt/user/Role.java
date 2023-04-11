ce8b845c5561b16d0d2a30877be4cbcb8669e5b3
Roberto Valdez <roberto.valdezisc@outlook.com>
2023-04-11 11:56:14 -0600

    first commit

---
+++ src/main/java/com/scurity/jwt/user/Role.java
@@ -0,0 +1,8 @@
+package com.scurity.jwt.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+
+}
---
+++ src/main/java/com/scurity/jwt/user/User.java
@@ -0,0 +1,59 @@
+package com.scurity.jwt.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
